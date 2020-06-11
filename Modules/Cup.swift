8377cac5c0abc5462cf60d621657dc9a3ba694f9
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-06-11 19:14:09 +0300

    Create Cup.swift

---
+++ Modules/Cup.swift
@@ -0,0 +1,46 @@
+// cup contains the 13 dice for the game
+public class Cup {
+    private var dice: [Dice]
+    var currentDice: [Dice]
+
+    public init() {
+        dice = [Dice]()
+        for i in 1...13 {
+            if (i < 7) {
+                dice.append(GreenDice.init())
+            }
+            else if (i < 11) {
+                dice.append(YellowDice.init())
+            }
+            else {
+                dice.append(RedDice.init())
+            }
+        }
+        currentDice = dice
+    }
+    
+    public func pickDice() -> [Dice]? {
+        if currentDice.count == 0 {
+            return nil
+        }
+        
+        var result = [Dice]()
+        for _ in 0..<min(3, currentDice.count) {
+            currentDice.shuffle()
+            result.append(currentDice.removeFirst())
+        }
+        
+        return result
+    }
+    
+    public func addRunner(runner: Dice?) {
+        guard let toAdd = runner else {
+            return
+        }
+        currentDice.append(toAdd)
+    }
+    
+    public func reset() {
+        currentDice = dice
+    }
+}
