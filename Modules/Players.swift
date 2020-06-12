313e9da1b750381230b88d4b739cd0412a913fad
Svetlin12 <43857986+Svetlin12@users.noreply.github.com>
2020-06-12 17:05:07 +0300

    Update Players.swift

--- Modules/Players.swift
+++ Modules/Players.swift
@@ -1,13 +1,23 @@
-import Player
-
-public class Players {
+public class Players: Sequence {
     var players: [Player]
     
-    init (players: Player...) {
+    public init () {
+        players = [Player]()
+    }
+    
+    public init (_ players: [Player]) {
         //error
 //        if players.count < 2 || players.count > 8 {
 //            return
 //        }
         self.players = players
+    }
+    
+    public func makeIterator() -> IndexingIterator<[Player]> {
+        return players.makeIterator()
+    }
+    
+    public func addPlayer(_ player: Player) {
+        players.append(player)
     }
 }
